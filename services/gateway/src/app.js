a551dbc705ad6eaa5f10c09fe15dd96395908535
eSuardOdin <esuardodin@gmail.com>
2026-02-02 13:07:44 +0100

    Working on gateway + brunp

--- services/gateway/src/app.js
+++ services/gateway/src/app.js
@@ -3,15 +3,18 @@ import userRouter from './routes/user.routes.js'
 import inventoryRouter from './routes/inventory.routes.js';
 import heroRouter from './routes/hero.routes.js';
 import logRouter from './routes/log.routes.js';
+import dungeonRouter from './routes/dungeon.routes.js';
+
 
 const app = express();
 
 app.use(express.json());
 
 app.use('/user', userRouter);
-app.use('/inventory', inventoryRouter);
-app.use('/heroes', heroRouter);
+app.use('/api/inventory', inventoryRouter);
+app.use('/api/heroes', heroRouter);
 app.use('/api/logs', logRouter);
+app.use('/api/dungeons', dungeonRouter);
 
 
 
